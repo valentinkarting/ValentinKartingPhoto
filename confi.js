ab05d4cfab706dd0680d44fbacf1e87684e7db6c
valentinkarting <lrpromatech@wanadoo.fr>
2025-02-16 11:48:38 +0100

    jkh

--- confi.js
+++
@@ -1,8 +0,0 @@
-// Affichage de la dernière mise à jour
-document.getElementById("dateMaj").textContent = new Date().toLocaleDateString();
-
-// Fonction pour accepter la politique
-function acceptPolicy() {
-    alert("Vous avez accepté la politique de confidentialité.");
-    localStorage.setItem("policyAccepted", "true");
-}
