c8318d788f97bbccbfa508731bf250e98c9d3508
valentinkarting <lrpromatech@wanadoo.fr>
2026-03-05 17:40:29 +0100

    12

--- service-worker.js
+++ service-worker.js
@@ -2,3 +2,10 @@ self.addEventListener('install', (event) => {
   console.log('Service Worker installé');
 });
 
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activé');
+});
+
+self.addEventListener('fetch', (event) => {
+  // Pas d'interception spécifique, on laisse le navigateur gérer les requêtes normalement
+});
