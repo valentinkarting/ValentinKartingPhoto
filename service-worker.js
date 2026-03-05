b45e18b235e30f565fbb4858490fb2166d1fa3e3
valentinkarting <lrpromatech@wanadoo.fr>
2026-03-05 11:42:55 +0100

    maj 05/03/2026 5

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installé');
-});
-
-self.addEventListener('fetch', (event) => {
-  // Ici tu peux gérer la mise en cache si tu veux
-});
