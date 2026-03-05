7c048196ff7f775c96a7024f14f58160737c4574
valentinkarting <lrpromatech@wanadoo.fr>
2026-03-05 17:18:29 +0100

    9

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,4 @@
-self.addEventListener('install', function(event) {
-  console.log("Service Worker installé");
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installé');
 });
+
